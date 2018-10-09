d311692bd6931ee97c17e3b7c501e68f07e99902
Vachounet <vachounet@live.fr>
2018-10-09 16:16:24 +0200

    sdm845-common: set client package name
    * Introduce a new boot receiver that force camera client package name via HIDL HAL to fix portrait mode

--- common.mk
+++ common.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Camera
+ PRODUCT_PACKAGES += \
+     CameraPackage
+     
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
